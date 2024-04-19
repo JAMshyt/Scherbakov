b1adf3eaa8282bc06493a604a716b17c5842bb42
stu-pksp120 <stu-pksp120@AMDVLA3511GW07>
2024-04-19 10:06:51 +0300

    add sort, writing in file and array

--- Scherbakov/Program.cs
+++ Scherbakov/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Scherbakov
 {
@@ -18,23 +19,78 @@ namespace Scherbakov
 
         class SubjectControl
         {
-            public void Sort()
+            public Subject[] Sort(Subject[] subjects)
             {
-
+                var newSubj = from i in subjects
+                                     orderby i.FormAttest, i.Title
+                                     select i;
+                return newSubj.ToArray();
             }
 
-            public void WriteInfile()
+            public void WriteInfile(Subject[] subjects)
             {
-
+                using (StreamWriter writer = new StreamWriter("..//..//result.txt",false))
+                {
+                    foreach (var i in subjects) 
+                    {
+                        writer.WriteLine(i.Title+" "+i.Semester+" "+i.FormAttest);
+                    }
+                }
             }
 
         }
 
         static void Main(string[] args)
         {
-            SubjectControl control = new SubjectControl();
+            int count;
+            for (; true;)
+            {
+                try
+                {
+                    Console.Write("введите количеств предметов:");
+                    count = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка, только целое число, попробуйте снова");
+                }
+            }
+            Subject[] subjects = new Subject[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                subjects[i] = new Subject();
+                Console.Write("Введите название " + (i + 1) + " предмета:");
+                subjects[i].Title = Console.ReadLine();
+
+                for (; true;)
+                {
+                    try
+                    {
+                        Console.Write("Введите семестр " + (i + 1) + " предмета:");
+                        subjects[i].Semester = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\nСеместр длжен быть цифрой, попробуйте ещё\n");
+                    }
+                }
+
+                Console.Write("Введите форму аттестации " + (i + 1) + " предмета:");
+                subjects[i].FormAttest = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            SubjectControl contoller = new SubjectControl();
+            contoller.WriteInfile(contoller.Sort(subjects));
 
 
+            foreach (var i in contoller.Sort(subjects))
+            {
+                Console.WriteLine(i.Title + " " + i.Semester + " " + i.FormAttest);
+            }
         }
     }
 }
